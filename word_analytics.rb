67801e6469a8f799b5ee5eebc8e42224e3f13ee0
macheteshuffle <jeff.li029@gmail.com>
2013-12-14 12:35:18 -0500

    added get_top_three method that lists top 3 of words, letters or miscellaneous characters used

--- word_analytics.rb
+++ word_analytics.rb
@@ -6,7 +6,7 @@ class WordAnalytics
   end
 
   def seperate_words
-    @input.downcase.gsub(/[,'.?"!@#$%&*()]/,'').split(' ')
+    @input.downcase.gsub(/[,;'.?"!@#$%&*()]/,'').split(' ')
   end
 
   def seperate_letters
@@ -27,47 +27,23 @@ class WordAnalytics
     number_of_type
   end
 
-
-  # def get_word_numbers
-  #   word_array = seperate_words
-  #   word_hash = word_array.group_by{ |i| i }
-  #   number_of_words = {}
-  #   word_hash.each do |key, word|
-  #     number_of_words[key] = word.size
-  #     puts "#{key}: #{number_of_words[key]}"
-  #   end
-  #   number_of_words
-  # end
-
-  # def get_letter_numbers
-  #   letter_array = seperate_letters
-  #   letter_hash = letter_array.group_by{ |i| i }
-  #   number_of_letters = {}
-  #   letter_hash.each do |key, letter|
-  #     number_of_letters[key] = letter.size
-  #     puts "#{key}: #{number_of_letters[key]}"
-  #   end
-  #   number_of_letters
-  # end
-
-  # def get_char_numbers
-  #   char_array = seperate_chars
-  #   char_hash = char_array.group_by{ |i| i }
-  #   number_of_chars = {}
-  #   char_hash.each do |key, char|
-  #     number_of_chars[key] = char.size
-  #     puts "#{key}: #{number_of_chars[key]}"
-  #   end
-  #   number_of_chars
-  # end
-
+  def get_top_three(occurrences)
+    top_three = occurrences.sort_by {|type, count| count}.reverse
+    puts "Top 3: "
+    top_three[0,3].each do |type|
+      puts "#{type[0]}: #{type[1]}"
+    end
+    top_three[0,3]
+  end
 end
 
-sentence = WordAnalytics.new('Hello, Jeff.')
+sentence = WordAnalytics.new('What a terrible and horrible and cruel, cruel, cruel world.')
 words = sentence.seperate_words
 letters = sentence.seperate_letters
 characters = sentence.seperate_chars
-sentence.get_occurrences(words)
-sentence.get_occurrences(letters)
-sentence.get_occurrences(characters)
+word_occurrence = sentence.get_occurrences(words)
+letter_occurrence = sentence.get_occurrences(letters)
+char_occurrence = sentence.get_occurrences(characters)
+sentence.get_top_three(letter_occurrence)
+
 
--- word_analytics_spec.rb
+++ word_analytics_spec.rb
@@ -31,4 +31,17 @@ describe WordAnalytics do
     expect(analytics.get_occurrences(characters)).to eql({','=>1,'.'=>1})
   end
 
+  it 'lists top 3 most common words' do
+    analytics = WordAnalytics.new("What a terrible and horrible and cruel, cruel, cruel world.")
+    words = analytics.seperate_words
+    occurrences = analytics.get_occurrences(words)
+    expect(analytics.get_top_three(occurrences)).to eql([["cruel", 3], ["and", 2], ["world", 1]])
+  end
+
+  it 'lists top 3 most common letters' do
+    analytics = WordAnalytics.new("What a terrible and horrible and cruel, cruel, cruel world.")
+    letters = analytics.seperate_letters
+    occurrences = analytics.get_occurrences(letters)
+    expect(analytics.get_top_three(occurrences)).to eql([["r", 8], ["l", 6], ["e", 6]])
+  end
 end
